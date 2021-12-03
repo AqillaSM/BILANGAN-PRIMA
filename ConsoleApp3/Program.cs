6dc7d3f7bd9513a0a2b6926df1c8db5eaab8c48a
userrr <userrr@DESKTOP-4K42564>
2021-12-03 10:46:35 +0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input angka : ");
+            int Angka = Convert.ToInt16(Console.ReadLine);
+
+            if (Angka < 2)
+            {
+                Console.WriteLine("Bukan Bilangan Prima");
+            }
+            else
+            {
+                int Perulangan = 2;
+                while (Angka % Perulangan != 0)
+                {
+                    Perulangan++;
+                }
+
+                if (Angka == Perulangan)
+                {
+                    Console.WriteLine("Bilangan Prima");
+                }
+                else
+                {
+                    Console.WriteLine("Bukan Bilangan Prima");
+                }
+            }
+
+
+        }
+    }
+}
